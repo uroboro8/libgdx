f9085faa9d14dded43e0ec3f72a081a162b35b50
Andrei Negruta <andrei.n@hotmail.it>
2022-04-30 15:35:41 +0200

    aggiornamento

--- gioco/game/core/src/it/labgaming/gamelab/Cespuglio.java
+++ gioco/game/core/src/it/labgaming/gamelab/Cespuglio.java
@@ -4,9 +4,9 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class Cespuglio extends BaseActor{
 
-    public Cespuglio(float x, float y, Stage s) {
+    public Cespuglio(float x, float y, Stage s)
+    {
         super(x, y, s);
-
         loadTexture("cespuglio.png");
         setBoundaryPolygon(8);
     }
